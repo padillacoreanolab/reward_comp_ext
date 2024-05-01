9bc6d44091fda18b4ac66416486bfa4611e32e92
ryoiwata <ryoiwata@github.com>
2024-05-01 16:40:49 -0400

    mountainsort correctly sorting with spike interface 0_98_2

---
+++ bin/spike_interface_0_98_2.sh
@@ -0,0 +1,9 @@
+conda create -n spike_interface_0_98_2 python=3.9 --yes
+conda activate spike_interface_0_98_2
+conda install -c conda-forge jupyterlab --yes
+
+# for spike sorting and preprocessing
+# https://spikeinterface.readthedocs.io/en/latest/installation.html
+pip install spikeinterface[full,widgets]==0.98.2
+# For spike sorting
+pip install --upgrade mountainsort5
