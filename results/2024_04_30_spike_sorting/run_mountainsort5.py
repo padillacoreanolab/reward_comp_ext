18426d904d39818d1b9e867bb7ef4cfe7e01676f
ryoiwata <ryoiwata@github.com>
2024-05-07 13:22:29 -0400

    backup spike sorting script

--- results/2024_04_30_spike_sorting/run_mountainsort5.py
+++ results/2024_04_30_spike_sorting/run_mountainsort5.py
@@ -5,6 +5,7 @@ import os
 import time
 import glob
 import warnings
+import numpy as np
 
 import matplotlib.pyplot as plt
 
@@ -32,36 +33,41 @@ print("Number of recording files: ", len(all_recording_files))
 for recording_file in all_recording_files:
     
     print("Current recording file: ", recording_file)
-    try:
-        trodes_recording = se.read_spikegadgets(recording_file, stream_id="trodes")       
-        trodes_recording = trodes_recording.set_probes(probe_object)
-        recording_basename = os.path.basename(recording_file)
-        recording_output_directory = "/scratch/back_up/reward_competition_extention/proc/spike_sorting/{}".format(recording_basename)
-        
-        os.makedirs(recording_output_directory, exist_ok=True)
-        print("Output directory: {}".format(recording_output_directory))
-        child_spikesorting_output_directory = os.path.join(recording_output_directory,"ss_output")
+    
+    trodes_recording = se.read_spikegadgets(recording_file, stream_id="trodes")       
+    trodes_recording = trodes_recording.set_probes(probe_object)
+    recording_basename = os.path.basename(recording_file)
+    recording_output_directory = "/scratch/back_up/reward_competition_extention/proc/spike_sorting/{}".format(recording_basename)
+    
+    os.makedirs(recording_output_directory, exist_ok=True)
+    print("Output directory: {}".format(recording_output_directory))
+    child_spikesorting_output_directory = os.path.join(recording_output_directory,"ss_output")
 
-        with open('successful_sorting.txt', "r") as myfile:
-            if recording_file in myfile.read():
-                warnings.warn("""Directory already exists for: {}. 
-                Either continue on if you are satisfied with the previous run 
-                or delete the directory and run this cell again""".format(recording_file))
+    try:
+        with open('successful_files.txt', "r") as myfile:
+            if recording_basename in myfile.read():
+                warnings.warn("""Directory already exists for: {}.
+                            Either continue on if you are satisfied with the previous run 
+                            or delete the directory and run this cell again""".format(recording_basename))
                 continue
+    except:
+        pass
+
+    try:
 
         start = time.time()
         # Make sure the recording is preprocessed appropriately
         # lazy preprocessing
 
-        recording_filtered = sp.bandpass_filter(trodes_recording, freq_min=300, freq_max=6000)
-        recording_preprocessed: si.BaseRecording = sp.whiten(recording_filtered, dtype='float32')
+        recording_filtered = sp.bandpass_filter(trodes_recording, freq_min=300, freq_max=6000, dtype=np.float32)
+        recording_preprocessed: si.BaseRecording = sp.whiten(recording_filtered)
 
         spike_sorted_object = ms5.sorting_scheme2(
             recording=recording_preprocessed,
             sorting_parameters=ms5.Scheme2SortingParameters(
                 detect_sign=0,
-                phase1_detect_channel_radius=700,
-                detect_channel_radius=700,
+                phase1_detect_channel_radius=2000,
+                detect_channel_radius=2000,
                 # other parameters...
                 )
                     )
@@ -82,24 +88,32 @@ for recording_file in all_recording_files:
         we_spike_sorted = si.extract_waveforms(recording=recording_preprocessed, 
             sorting=spike_sorted_object, folder=waveform_output_directory,
             ms_before=1, ms_after=1, progress_bar=True,
-            n_jobs=8, total_memory="1G", overwrite=True,
+            n_jobs=8, total_memory="16G", overwrite=True,
             max_spikes_per_unit=2000)
         
         phy_output_directory = os.path.join(recording_output_directory, "phy")
         
-        export_to_phy(we_spike_sorted, phy_output_directory,
-            compute_pc_features=True, compute_amplitudes=True, remove_if_exists=False)
+        export_to_phy(we_spike_sorted, 
+                      phy_output_directory,
+                      compute_pc_features=True, 
+                      compute_amplitudes=True, 
+                      remove_if_exists=False)
             
-        with open('successful_sorting.txt', "a") as myfile:    
-            print("Successfully sorted: ", recording_file)
-            myfile.write("\n" + recording_file)
-    
+        # edit the params.py file os that it contains the correct realtive path
+        params_path = os.path.join(phy_output_directory, "params.py")
+        with open(params_path, 'r') as file:
+            lines = file.readlines()
+        lines[0] = "dat_path = r'./recording.dat'\n"
+        with open(params_path, 'w') as file:
+            file.writelines(lines)                   
+
+        
     except Exception as e: 
         print(e)
         
-        print("Failed sorting: ", recording_file)
-        with open("failed_sorting.txt", "a") as myfile:
-            myfile.write("\n" + recording_file)
+        print("Failed sorting: ", recording_basename)
+        with open("failed_files.txt", "a") as myfile:
+            myfile.write("\n" + recording_basename)
 
 def main():
     """
