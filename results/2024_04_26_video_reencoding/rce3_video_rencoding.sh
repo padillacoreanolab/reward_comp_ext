47fe69cc2df8b17c828cbee203a6ad01e9f45118
ryoiwata <ryoiwata@github.com>
2024-06-11 13:12:20 -0400

    chore: reencode video without reformatting size

--- results/2024_04_26_video_reencoding/rce3_video_rencoding.sh
+++ results/2024_04_26_video_reencoding/rce3_video_rencoding.sh
@@ -10,22 +10,15 @@ for full_path in ${video_directory}/*/*/*.h264; do
     echo Currently working on: ${full_path}
     file_name=${full_path##*/}
     base_name="${file_name%.h264}"
+    mp4_out_path=${output_directory}/${base_name}.fixed.mp4
 
     echo Directory: ${dir_name}
     echo File Name: ${file_name}
     echo Base Name: ${base_name}
-
-    mp4_out_path=${output_directory}/${base_name}.original.mp4
     echo Converting h264 to mp4
-    
-    ffmpeg -n \
-    -i ${full_path} \
-    -filter:v \
-    "scale=1280:720:flags=lanczos" \
-    -c:a copy ${mp4_out_path}
-
     echo Reencoding mp4
-    ffmpeg -n -i ${mp4_out_path} -c:v libx264 -pix_fmt yuv420p -preset superfast -crf 23 ${output_directory}/${base_name}.fixed.mp4
+    
+    ffmpeg -n -i ${full_path} -c:v libx264 -pix_fmt yuv420p -preset superfast -crf 23 ${mp4_out_path}
 
 done
 
