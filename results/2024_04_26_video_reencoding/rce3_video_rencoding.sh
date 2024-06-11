cc0295222a25d35cd28268b091e5c66a073e92e2
ryoiwata <ryoiwata@github.com>
2024-06-11 14:09:35 -0400

    script: final reencoding

--- results/2024_04_26_video_reencoding/rce3_video_rencoding.sh
+++ results/2024_04_26_video_reencoding/rce3_video_rencoding.sh
@@ -10,15 +10,14 @@ for full_path in ${video_directory}/*/*/*.h264; do
     echo Currently working on: ${full_path}
     file_name=${full_path##*/}
     base_name="${file_name%.h264}"
-    mp4_out_path=${output_directory}/${base_name}.fixed.mp4
 
     echo Directory: ${dir_name}
     echo File Name: ${file_name}
     echo Base Name: ${base_name}
     echo Converting h264 to mp4
+
     echo Reencoding mp4
-    
-    ffmpeg -n -i ${full_path} -c:v libx264 -pix_fmt yuv420p -preset superfast -crf 23 ${mp4_out_path}
+    ffmpeg -n -i ${full_path} -c:v libx264 -pix_fmt yuv420p -preset superfast -crf 23 ${output_directory}/${base_name}.fixed.mp4
 
 done
 
