de43cb18b12bd8af7e0436fdc6864ec0d9f90844
ryoiwata <ryoiwata@github.com>
2024-08-01 13:04:17 -0400

    nb: bar graph cleaning up power

--- bin/pymer4_0_8_2.sh
+++ bin/pymer4_0_8_2.sh
@@ -1,8 +1,8 @@
 mamba create --name pymer4_0_8_2 -c ejolly -c conda-forge -c defaults pymer4  --yes
-mamba activate pymer4
+mamba activate pymer4_0_8_2
 
 mamba install -c conda-forge jupyterlab --yes
-
+# mamba install conda-forge::ipykernel --yes
 
 
 
